153798d69211f276d01a856c1b41be642278b421
yoocky <me@yoocky.com>
2014-10-14 14:01:03 +0800

    modified:   libs/router.js

--- libs/router.js
+++ libs/router.js
@@ -1,6 +1,46 @@
 /**
+ *版本信息
  * @fileOverview  基于hash的页内路由插件
- * @autor Yoocky
+ * @autor Yoocky <me@yoocky.com>
+ * @ version 1.0
+ *
+ *功能描述
+ * 1、支持历史记录，响应浏览器的前进后退
+ * 2、支持<a href="#controller" ></a> 格式跳转
+ * 3、支持open("controller")方法打开页面
+ * 4、支持设置默认首页，
+ * 5、支持带hash值的url直接打开对应页面
+ * 6、支持页面切换绑定事件
+ * 7、暂不支持页面切换自定义动画效果
+ *
+ *配置说明
+ *@param conroller type Object
+ *      数据格式demo :
+ *          {
+ *            "index" : {
+ *                  title : "页面的title",
+ *                  before: function(){},//打开前的回调
+ *                  after: function(){}//打开后的回调
+ *              }
+ *          }
+ *          为空对象或者未设置时会自动遍历初始化
+ *@param index type String 
+ *       demo "index"
+ *       为空时自动在页面上取第一个有效的cotroller
+ *@param wrap type String 
+ *       默认为"singlePage" 
+ *       路由有效范围的属性标志
+ *@param pageFlag type String
+ *       默认为"page"
+ *       存放controller的属性标志
+ *@param beforeFn type Function
+ *       全局的切换页面前的回调
+ *@param refreshFn type Function
+ *       通过路由刷新页面的回调
+ *@param changeFn type Function
+ *       全局切换页面后的回调
+ *@param awaysFn type Function
+ *       刷新切换后的总是回调
  */
 ; (function(w) {
     function router(options) {
@@ -9,7 +49,7 @@
         var defaults = {
             wrap: 'singlePage',
             pageFlag: 'page',
-            before: function() {},
+            beforeFn: function() {},
             refreshFn: function() {},
             changeFn: function() {},
             awaysFn: function() {},
@@ -22,7 +62,6 @@
         that.path = that._initPath();
         that._hashChange();
         that.open();
-
     }
     //原型上的一些方法
     router.prototype = {
@@ -78,7 +117,7 @@
                 if (current.length) {
                     var _to = this.controller[to];
                     var from = this.path.curPage;
-                    this.before();
+                    this.beforeFn();
                     //执行页面打开前私有回调
                     if ($.isFunction(_to.before)) {
                         _to.before(from);
